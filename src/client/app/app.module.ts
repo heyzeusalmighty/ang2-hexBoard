8be6c1ddc08f15ce82f16f54483f2bd07b712930
Thomas Lombardi <thomas.lombardi@wholefoods.com>
2016-10-31 21:55:58 -0500

    more things, more stuff, more all the time

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdInputModule } from '@angular2-material/input';
+import { MdListModule } from '@angular2-material/list';
+import { MdTabsModule } from '@angular2-material/tabs';
 
 import { AppComponent } from './app.component';
 import { GameScreenComponent } from './game-screen/game-screen.component';
 import { HexGridComponent } from './hex-grid/hex-grid.component';
 import { HexTileComponent } from './hex-tile/hex-tile.component';
 import { HeaderComponent } from './header/header.component';
+import { PlayerBarComponent } from './player-bar/player-bar.component';
+
 
 @NgModule({
   declarations: [
@@ -15,12 +22,18 @@ import { HeaderComponent } from './header/header.component';
     GameScreenComponent,
     HexGridComponent,
     HexTileComponent,
-    HeaderComponent
+    HeaderComponent,
+    PlayerBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule.forRoot(),
+		MdCardModule.forRoot(),
+		MdTabsModule.forRoot(),
+		MdInputModule.forRoot(),
+		MdListModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/client/app/player-bar/player-bar.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PlayerBarComponent } from './player-bar.component';
+
+describe('Component: PlayerBar', () => {
+  it('should create an instance', () => {
+    let component = new PlayerBarComponent();
+    expect(component).toBeTruthy();
+  });
+});
