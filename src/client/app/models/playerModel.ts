c63c33cde8313ab9378b11039e57590533a7eb50
Thomas Lombardi <thomas.lombardi@gmail.com>
2016-11-03 07:56:17 -0500

    moneybox stuff

--- src/client/app/models/playerModel.ts
+++ src/client/app/models/playerModel.ts
@@ -1,12 +1,12 @@
 import { TrackTile } from './trackTile';
-import { ShipGroup } from './shipgroup';
+import { ShipGroup } from './shipGroup';
 import { Player } from './player';
 import { TurnStatus } from './status';
 
 export class PlayerModel {
-    currentPlayer: Player;
-    scienceTrack: TrackTile[];
-    ships: ShipGroup;
-    upgrades: Array<any>;
-    status: TurnStatus;
+	currentPlayer: Player;
+	scienceTrack: TrackTile[];
+	ships: ShipGroup;
+	upgrades: Array<any>;
+	status: TurnStatus;
 }
